1d151ce7f3877a17e070d2af5c664e3d788d42ab
Hagay Elbaz <72602938+hagayelbaz@users.noreply.github.com>
2021-05-07 16:11:53 +0300

    Add files via upload

---
+++ CreateRandomDesktopBackGround/CreateRandomDesktopBackGround/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using Application = System.Windows.Forms.Application;
+
+namespace CreateRandomDesktopBackGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //run application while desktop setup
+            //Create a registry key, then save your program path
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("CreateRandomDesktopBackGround.exe", Application.ExecutablePath.ToString());
+
+            //hide window from user
+            var handle = GetConsoleWindow();
+            ShowWindow(handle , 0);
+                               
+            //random picture from website
+            string url = string.Format("https://kbdevstorage1.blob.core.windows.net/asset-blobs/{0}_en_1", new Random().Next(19000, 19980));
+            // picture path
+            string main_path = Environment.GetFolderPath(Environment.SpecialFolder.Cookies);
+            string path = main_path + @"\image.png";
+            //replace picture
+            using (WebClient client = new WebClient())
+            {
+                try { client.DownloadFile(new Uri(url), main_path + @"\image1.png");}
+                catch { return; }
+
+                //delete for replace picture
+                if (File.Exists(path))
+                    File.Delete(path);
+
+                //rename picture file to image.png
+                File.Move(main_path + @"\image1.png", path);
+            }
+            //set desktop background
+            set_Desktop_Background(path);       
+            return ;
+        }
+        //set desktop background
+        private static void set_Desktop_Background(string sFile_FullPath)
+        {
+            win32.SystemParametersInfo(20, 0, sFile_FullPath, 1 | 2);
+        }
+        //helpes for hide window
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }  
+    // for esktop backcolor
+    internal sealed class win32
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern int SystemParametersInfo(int uAction, int uParam, String lpvParam, int fuWinIni);
+    }    
+}
---
+++ CreateRandomDesktopBackGround/CreateRandomDesktopBackGround/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CreateRandomDesktopBackGround")]
+[assembly: AssemblyDescription("This app creates a random background for your computer, you only need to start it for the first time, then with each startup of the computer - the background will change automatically.\nIf you are not happy with the background - you can click on the app - and it will change.\nHope you enjoy!!!!!\nThank you very much.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreateRandomDesktopBackGround")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9c8f273-f676-4643-be90-9504a5280000")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
